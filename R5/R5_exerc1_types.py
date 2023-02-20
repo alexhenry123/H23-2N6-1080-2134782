2de8246751162bf2be2a100a899d027eeb75d1ca
Alex Henry <2134782@cegepmontpetit.ca>
2023-02-19 21:18:45 -0500

    Progres

--- R5/R5_exerc1_types.py
+++ R5/R5_exerc1_types.py
@@ -1,6 +1,3 @@
-from curses.ascii import isdigit
-import numbers
-from sre_compile import isstring
 import utilitaire_gen_Q as util_r5
 # Q1 Ce petit script demande l'âge de la personne et imprime sa date de naissance.
 # La fonction input() retourne une valeur en string.
@@ -10,16 +7,18 @@ import utilitaire_gen_Q as util_r5
 # Indice : pour pouvez utiliser la fonction  dir(str)  pour obtenir toutes les méthodes
 # des objets str... une de ces méthodes devrait vous aider... vous ne trouverez pas la méthode exacte dans vos notes de cours. 
 print(f"Q1{80*'_'}")
-age = input("Entrez votre âge : ")
-if age == isstring:
+age = 0
+input("Entrez votre âge : ") == age 
+if isinstance(age, int):
+    print(util_r5.annee_de_naissance(age))
+elif isinstance(age, str):
     print ("ERREUR - Âge non valide")
-elif age == isdigit:
-    print(util_r5.annee_de_naissance(age))    
-    
+else:
+    print("ERREUR - Rentrez un âge valide")
+   
 #Ne compile pas tel que souhaité
 
 # # Q2 Des variables sont générées par une fonction dans un module utilitaire.
 # # Imprivez la valeur de chaque variable et son type
 print(f"Q2{80*'_'}")
-var1,var2,var3,var4,var5,var6 = util_r5.generation_variable()
-
+# var1,var2,var3,var4,var5,var6 = util_r5.generation_variable()
