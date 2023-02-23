2d51a459c01d471ee08ea64be19530d93d8f2baa
Henry Alex <2134782@cegepmontpetit.ca>
2023-02-23 13:42:06 -0500

    Progres

--- R10/R10_Exercices_Depart/Ex1_CEM_GetFromFakestoreApi_v1.py
+++ R10/R10_Exercices_Depart/Ex1_CEM_GetFromFakestoreApi_v1.py
@@ -1,3 +1,4 @@
+from operator import index
 import requests
 import json
 
@@ -17,31 +18,38 @@ base_url="https://fakestoreapi.com"
 #          en précisant le nombre de carts qui avait été demandé
 #   Si on passe autre chose qu'un nombre entier en paramètre 
 #          faites un message comme quoi il faut entrer un nombre entre 1 et 10
-
 #   La fonction doit retourner le nombre de carts demandés, si le nombre est entre 1 et 10
-def request_carts(nb_de_carts=None):
+
+
+#Requête de fakestoreapi
+res = requests.get(f'{base_url}/carts')
+donnees_json = json.dumps(res.json(),indent=4)
+compteur = 0
+for carts in donnees_json:
+    if carts == "id":
+        compteur+=1
+nb_de_carts = compteur
+
+#Création de la méthode
+def request_carts(nb_de_carts):
+    
+    #S'il n'y a pas de paramètre entré
     if nb_de_carts is None:        
         print("Veuillez entrer un paramètre, soit le nombre de carts")
-    elif nb_de_carts < 0 and nb_de_carts > 10:
+                
+    #Si le paramètre n'est pas entre 1 et 10
+    if nb_de_carts <= 0 and nb_de_carts >= 11:
         print(f"Votre nombre entré, soit {nb_de_carts}, ne correspond pas. Veuillez entrer un nombre entre 1 et 10 pour le nombre de carts")
-    elif nb_de_carts > 0 and nb_de_carts <= 10:
         
-    
-request_carts()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #(Valide) Si le paramètre donné est un entier entre 1 et 10
+    elif nb_de_carts >= 1 and nb_de_carts <= 10:
+        print(f"Voici le nombre de carts demandés : {nb_de_carts}")
+        
+    #Dans les autres cas
+    else:
+        print(f"Veuillez entrer un nombre entier entre 1 et 10")
+        
+request_carts(nb_de_carts)
 
 # Q2 Écrivez une fonction appelée request_products qui aura 1 paramètre
 #   soit le nombre de produits que vous voulez aller chercher
