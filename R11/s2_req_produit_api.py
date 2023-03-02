98fceeda2e27d7e8b540fcec387c27fef3aff96a
Alex H <123370992+alexhenry123@users.noreply.github.com>
2023-03-02 12:28:50 -0500

    Cleanup

---
+++ R11/s2_req_produit_api.py
@@ -0,0 +1,21 @@
+import os
+os.chdir(os.path.dirname(__file__)) # Cette ligne fait que l'exécution du script aura toujours lieu dans le répertoire où il se trouve.
+
+# Maintenant que nous avons un script capable de lire et décoder le fichier csv.
+# Nous voulons ajouter des informations à liste de produits de chaque client :
+# le prix de chaque produit et sa catégorie.
+
+# Les informations sur les produits proviennent du site du magasin.
+# Vous devez aller chercher les informations à l'aide du module requests.
+url = "https://fakestoreapi.com/"
+
+import echantillon_data
+import s1_lire_csv_ventes
+import json
+import requests as rq
+ls_client_s1 = echantillon_data.liste_clients_s1
+ls_client_s2 = echantillon_data.liste_clients_s2
+
+res = rq.get(f"{url}/products")
+donnees_json = json.dumps(res.json(),indent=4)
+print(donnees_json)
