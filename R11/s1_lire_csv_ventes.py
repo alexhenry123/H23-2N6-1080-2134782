ee468882ef91fc8c41b7fd88cb48beb49e6c97f8
Alex H <123370992+alexhenry123@users.noreply.github.com>
2023-03-02 21:24:47 -0500

    Progres

--- R11/s1_lire_csv_ventes.py
+++ R11/s1_lire_csv_ventes.py
@@ -13,21 +13,23 @@ os.chdir(os.path.dirname(__file__)) # Cette ligne fait que l'exécution du scrip
 
 # Le but ultime de ce script est d'arriver à une liste, contenant pour chaque client
 ls_clients = []
-dict_commandes = {"ID produit","Quantité"}
 with open('data_ventes.csv', 'r', encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file)
     for i in range(5):
         next(csv_reader)
     #Saut des lignes
     for line in csv_reader:
+        position = 0
         id_client, nom, prenom = line[0:3]
         for id in range(1,21):
             id_quantité = line[3:len(line)]
-            dict_commandes.update()
+            ls_commandes = [position,line[id]]
+            #id_quantité += ls_commandes
+            position += 1
         dict_client = {
-            "ID client":id_client,
-            "Nom client":nom,
-            "Prénom client":prenom,
-            "Commande":dict_commandes}
+            "ID client": id_client,
+            "Nom client": nom,
+            "Prénom client": prenom,
+            "Commande":{"ID produit": ls_commandes[0],"Quantité": ls_commandes[1]}}
         ls_clients += dict_client.items()
 print(ls_clients)
