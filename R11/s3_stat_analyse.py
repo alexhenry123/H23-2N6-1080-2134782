fb33e05bfa11aa29f9cbb9c946a94b6365e8a25a
Alex H <123370992+alexhenry123@users.noreply.github.com>
2023-03-03 20:39:52 -0500

    s3 1er commit

--- R11/s3_stat_analyse.py
+++ R11/s3_stat_analyse.py
@@ -16,3 +16,30 @@ os.chdir(os.path.dirname(__file__)) # Cette ligne fait que l'exécution du scrip
 # Par catégorie :
 #   Nombre total d'articles vendus
 #   Montant total des ventes
+import s1_lire_csv_ventes
+import s2_req_produit_api
+ls_client_s2 = s2_req_produit_api.ls_client
+dict_client_s2 = s2_req_produit_api.dict_prixcategorie
+
+#Variables
+total_articles_vendus = 0
+montant_total_ventes = 0
+prix_moyen_ventes = 0
+prix_moyen_ventes = 0
+plus_grand_prix = 0
+plus_petit_prix = 0
+
+#Nombre d'articles vendus
+for articles in ls_client_s2:
+    if articles == "id":
+        total_articles_vendus+=1
+
+#Montant total des ventes
+prix = 0
+for prix in dict_client_s2.values:
+    prix += dict_client_s2["price"]
+
+#Prix moyen des commandes
+prix_moyen_ventes = montant_total_ventes / total_articles_vendus
+
+
