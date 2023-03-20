ef8e15233bfaade2c984fd00abaeeb33ee4d847b
Alex H <123370992+alexhenry123@users.noreply.github.com>
2023-03-20 13:39:15 -0400

    Progres

---
+++ R15/r15_ex1.py
@@ -0,0 +1,23 @@
+class Compte:
+    def __init__(self,no_compte,type_compte,nip_client,solde = 0):
+        self.no_compte = no_compte
+        self.type_compte = type_compte
+        self.nip_client = nip_client
+        self.solde = solde
+    def deposer(self,montant_a_deposer):
+        solde += montant_a_deposer
+        return montant_a_deposer
+    def retirer(self,montant_a_retirer):
+        if montant_a_retirer <= montant_a_deposer:       
+            solde -= montant_a_retirer
+        else:
+            print(f'Votre solde est de {montant_a_deposer} : ce qui est inférieur au montant de {montant_a_retirer} que vous voulez retirer.')
+
+#Variables (à modifier)
+montant_a_deposer = 50
+montant_a_retirer = 3
+
+#Méthodes appelées
+compte_1 = Compte(1,"Administrateur",1234,0)
+compte_1.deposer(montant_a_deposer)
+compte_1.retirer(montant_a_retirer)
