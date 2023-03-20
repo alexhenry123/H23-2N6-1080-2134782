f99a19cca19a09411581b1f6dcec5ca3dbee9371
Alex H <123370992+alexhenry123@users.noreply.github.com>
2023-03-20 17:21:09 -0400

    Delete r15_ex3_voitures.py
    Remplacé par version plus récente

--- R15/r15_ex3_voitures.py
+++
@@ -1,5 +0,0 @@
-class Voiture:
-    def __init__(self) -> None:
-        self.etat_df = "Neuf"
-    def imprimer_infos():
-        print(f'{marque} {modele} {annee} de couleur {couleur}, avec seulement {kilometrage} km. Pour un prix raisonnable de {prix}$ (payable en 12 versements de 10.25$. Dans un état {etat}.')
