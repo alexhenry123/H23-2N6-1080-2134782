0e68b0c77eaadfec56b784499f93be111a0f818c
Alex H <123370992+alexhenry123@users.noreply.github.com>
2023-03-20 13:12:40 -0400

    Progres

--- R15/chaise.py
+++ R15/chaise.py
@@ -1,7 +1,9 @@
 class Chaise:
-    def __init__(self,h,l,p):
-        self.l = 5
+    def __init__(self,h,l,p) -> None:
+        self.l 
+        self.h
+        self.p
 
 chaise_1 = Chaise(5,6,7)
 
-print(chaise_1)
+print(chaise_1.l)
