2d3f4361881f0f7269a7a3510a81df9995435979
Alex H <123370992+alexhenry123@users.noreply.github.com>
2023-03-21 11:51:08 -0400

    Update final R15 - Ex3

--- R15/R15_Classes_Exercice/r15_ex3_final_voiture.py
+++ R15/R15_Classes_Exercice/r15_ex3_final_voiture.py
@@ -1,5 +1,5 @@
 class Voiture:
-    def __init__(self,modele,marque,annee,couleur,kilometrage,prix,etat) -> None:
+    def __init__(self,modele,marque,annee,couleur,kilometrage,prix,etat='neuf') -> None:
         self.modele = modele
         self.marque = marque
         self.annee = annee
@@ -7,10 +7,8 @@ class Voiture:
         self.kilometrage = kilometrage
         self.prix = prix
         self.etat = etat
-        self.etat_df = "Neuf"
     def imprimer_infos(voiture):
-        print(f'''{voiture.marque} {voiture.modele} {voiture.annee} de couleur {voiture.couleur}, avec seulement {voiture.kilometrage} km. 
-Pour un prix raisonnable de {voiture.prix}$ (payable en 12 versements de {voiture.prix / 12}$) dans un état {voiture.etat}!''')
+        print(f'{voiture.marque} {voiture.modele} {voiture.annee} de couleur {voiture.couleur}, avec seulement {voiture.kilometrage} km. Pour un prix raisonnable de {voiture.prix}$ (payable en 12 versements de {voiture.prix / 12}$) dans un état {voiture.etat}!')
 
 #Instanciation d'objets
 voiture_1 = Voiture(modele='Tercel',marque='Toyota',annee='1972',couleur='rouge',kilometrage='12888888',prix=123,etat='superbe')
