3bcbf10d34c73b45e73aa5d3e9fc30cddb162a2c
Henry Alex <2134782@cegepmontpetit.ca>
2023-03-23 13:47:25 -0400

    Ex1 - R16 : Incomplet

--- R16/R16-Exercices/r16_ex1.py
+++ R16/R16-Exercices/r16_ex1.py
@@ -12,13 +12,13 @@ class Joueur(Personne):
         self.position = position
 
 class Attaquant(Joueur):
-    def __init__(self, nom, prenom, date_naissance, no_chandail, position, nb_tirs_au_but,nb_assistance):
+    def __init__(self, nom, prenom, date_naissance, no_chandail, position, nb_tirs_au_but,nb_assistances):
         super().__init__(nom, prenom, date_naissance, no_chandail, position)
         self.nb_tirs_au_but = nb_tirs_au_but
-        self.nb_assistance = nb_assistance
+        self.nb_assistances = nb_assistances
     def compter_but():
         total_buts = 0
-        total_buts += Attaquant.nb_tirs_au_but + Attaquant.nb_assistance
+        total_buts += Attaquant.nb_tirs_au_but + Attaquant.nb_assistances
         return total_buts
 class Defenseur(Joueur):
     def __init__(self, nom, prenom, date_naissance, no_chandail, position, nb_interceptions,nb_passes):
@@ -30,18 +30,32 @@ class Gardien(Joueur):
         super().__init__(nom, prenom, date_naissance, no_chandail, position)
         self.nb_arrets = nb_arrets
         self.nb_tirs_essuyes = nb_tirs_essuyes
-        
-#Instanciations
+    def arreter_tir():
+            Gardien.nb_arrets+=1
+class Equipe(Joueur):
+    def __init__(self, nom_equipe, liste_joueurs) -> None:
+        self.nom_equipe = nom_equipe
+        self.liste_joueurs = liste_joueurs
+        liste_joueurs = []
+    def engager_joueur(joueur):
+        liste_joueurs += joueur
+    def ejecter_joueur(joueur):
+        liste_joueurs -= joueur
+
+#Instanciations:
+
+#Joueurs
 gardien_Logan_Ketterer = Gardien("Ketterer","Logan","9 novembre 1993","#1","gardien",128,208)
 defenseur_Zachary_Brault_Guillard = Defenseur("Brault-Guillard","Zachary","5 mars 1991","#15","défenseur",32,44)
 attaquant_Sunusi_Ibrahim = Attaquant("Ibrahim","Sunusi","1 octobre 2002","#14","attaquant",23,44)
+liste_joueurs_equipe1 = gardien_Logan_Ketterer, defenseur_Zachary_Brault_Guillard, attaquant_Sunusi_Ibrahim
+#Equipe
+equipe_CF_Montreal = Equipe("Équipe 1",liste_joueurs_equipe1)
 
-#Tests:
-
+ 
 #Test 1
 print(f"""Test 1 : {gardien_Logan_Ketterer.date_naissance}, {defenseur_Zachary_Brault_Guillard.date_naissance}, {attaquant_Sunusi_Ibrahim.date_naissance}
 """)
-
 #Test 2
 print(f"""Test 2 : 
 No chandail : 
@@ -49,8 +63,16 @@ No chandail :
 Positions :
 {gardien_Logan_Ketterer.position}, {defenseur_Zachary_Brault_Guillard.position}, {attaquant_Sunusi_Ibrahim.position}
 """)
-
 #Test 3 
-print(f"""Test 3 : """)
+print(f"""Test 3 : 
+Valeurs propres à chaque joueur :
+Attaquant : {attaquant_Sunusi_Ibrahim.nb_tirs_au_but}, {attaquant_Sunusi_Ibrahim.nb_assistances}
+Défenseur : {defenseur_Zachary_Brault_Guillard.nb_interceptions}, {defenseur_Zachary_Brault_Guillard.nb_passes}
+Gardien : {gardien_Logan_Ketterer.nb_arrets}, {gardien_Logan_Ketterer.nb_tirs_essuyes}
+""")
+#Test 4
+for joueur in equipe_CF_Montreal.liste_joueurs:
+    print(f"""Test 4 : 
+Liste de joueurs : {equipe_CF_Montreal.liste_joueurs}""")
 
 
