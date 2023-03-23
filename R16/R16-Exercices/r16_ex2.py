0eb4a35d63001b7b3afd37432d2395abb271b5c2
Henry Alex <2134782@cegepmontpetit.ca>
2023-03-23 14:33:23 -0400

    Progres Ex2

--- R16/R16-Exercices/r16_ex2.py
+++ R16/R16-Exercices/r16_ex2.py
@@ -15,6 +15,24 @@ class Voiture_electrique(Voiture):
         self.autonomie_max = autonomie_max
         self.autonomie_actuelle = autonomie_actuelle
         self.type_recharge = type_recharge
-    def recharger(temps):
-        if Voiture_electrique.autonomie_actuelle (temps / 2) * 40 <= Voiture_electrique.autonomie_max:
-            Voiture_electrique.autonomie_actuelle += (temps / 2) * 40
+    def recharger(voiture,temps):
+        if voiture.type_recharge == "Niveau 2":
+            if voiture.autonomie_actuelle + (temps / 120) * 40 <= voiture.autonomie_max:
+                voiture.autonomie_actuelle += (temps / 120) * 40
+        elif voiture.type_recharge == "Rapide(100kw)":
+            if voiture.autonomie_actuelle + (temps / 8) * 40 <= voiture.autonomie_max:
+                voiture.autonomie_actuelle += (temps / 8) * 40
+        elif voiture.type_recharge == "Rapide(300kw)":
+            if voiture.autonomie_actuelle + (temps / 2) * 40 <= voiture.autonomie_max:
+                voiture.autonomie_actuelle += (temps / 2) * 40
+
+#Instanciations
+auto_Paul = Voiture_electrique("Audi","Q8",2001,10,"jaune",68000,400,30,"Rapide(100kw)")
+auto_Lucie = Voiture_electrique("Chevrolet","Silverado",2023,10,"argent",86000,640,30,"Rapide(300kw)")
+
+#Tests
+print(f"Autonomie actuelle de l'auto : {auto_Lucie.autonomie_actuelle}")
+Voiture_electrique.recharger(auto_Lucie,10)
+print(f"Autonomie actuelle de l'auto après rechargement : {auto_Lucie.autonomie_actuelle}")
+
+
