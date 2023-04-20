7593b922285dca74fdd9d6fd800b81362801d762
Henry Alex <2134782@cegepmontpetit.ca>
2023-04-20 13:17:25 -0400

    Ex1 terminé

--- R21/R21 Exercices/R21 Exercice1 Cercle/R21_Exercice1_Cercle_DEPART.py
+++ R21/R21 Exercices/R21 Exercice1 Cercle/R21_Exercice1_Cercle_DEPART.py
@@ -1,6 +1,23 @@
 #Vous avez une classe Cercle qui a un attribut privé rayon (_rayon)
-
 class Cercle:
-    def __int__(self, rayon):
+    def __init__(self, rayon):
         self._rayon = rayon
-
+    @property
+    def rayon(self):
+        print("Get rayon")
+        return self._rayon
+    @rayon.setter
+    def rayon(self):
+        print("Set rayon")
+        return self._rayon
+    @rayon.deleter
+    def rayon(self):
+        del self._rayon
+        print("Delete rayon")
+    
+#Instanciations et tests
+cercle1 = Cercle(45)
+#print(cercle1.rayon)
+cercle1._rayon = 50
+del cercle1.rayon
+print(cercle1.rayon)
